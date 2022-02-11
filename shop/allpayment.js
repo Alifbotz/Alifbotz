603e9a731e3c843d2cee47edc019848c0474c5c7
Alifbotz <aluy70003@gmail.com>
2022-02-11 14:53:15 +0700

    ALIF2

---
+++ shop/allpayment.js
@@ -0,0 +1,34 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY Alifbotz
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0887 4350 473277
+2. BCA 
+	ㅁ 3901 0887 4350 473277
+3. BNI 
+	ㅁ 881 0887 4350 473277
+4. PERMATA
+	ㅁ 852 8887 4350 473277
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0819 1560 5137
+2. OVO
+	ㅁ 0819 1560 5137
+3. DANA
+	ㅁ 0819 1560 5137
+
+
+*📲 PULSA XL*
+
+    ㅁ 081915605137
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
